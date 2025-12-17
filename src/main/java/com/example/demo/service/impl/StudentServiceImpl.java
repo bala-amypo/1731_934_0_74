ccfee506312b0d364cda1501aff7ecc83e7a61f6
bala-amypo <bala@amypo.in>
2025-12-17 09:11:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -7,33 +7,38 @@ import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepo;
 import com.example.demo.service.StudentService;
 
-@service
+@Service
 public class StudentServiceImpl implements StudentService
 {
-@Autowired
-StudentRepo
+    @Autowired
+    StudentRepo stRepo;
 
-@Override
-public Student insertStudent(Student st)
-{
+    @Override
+    public Student insertStudent(Student st)
+    {
+             
+             stRepo.save(st);
+             return st;
+    }
 
-    stRepo.save(st);
-   
-    return st;
-}
- @Override
- public List<Student> getAllStudent()
- {
-    return stRepo.findAll();
- }
- @Override
- public Optional<Student> getOneStudent(Long id ) 
- {
-    return stRepo.findById(id);
- }
- @Override
- public void deleteStudent(Long id)
- {
-    stRepo.deleteById(id);
- }
+    @Override
+    public List<Student> getAllStudents()
+    {
+
+        return stRepo.findAll();
+    }
+
+    @Override
+    public Optional<Student> getOneStudent(Long id)
+    {
+        return stRepo.findById(id);
+    }
+
+    @Override
+    public void deleteStudent(Long id)
+    {
+         stRepo.deleteById(id);
+    }
+
+
 }
